694b9ffe554dc639c732549057f9d94efccfe864
Wei Ning <macgnw@gmail.com>
2019-03-24 12:06:03 +1100

    udemy

--- control_structures/looping.rb
+++ control_structures/looping.rb
@@ -30,7 +30,24 @@ def for_numeric_range
   print "\n"
 end
 
+# source: udemy compr ruby prog s2l8
+def times_loop 
+  nums = []
+  10.times do
+    nums.push(1)
+  end
+  p nums
+
+  # to manipulate a global variable
+  10.times do
+    $global_var = 123
+  end
+  p $global_var
+  # global var is bad; it does not have file scope
+end
+
 demo_while_loop
 demo_foreach
 demo_each
 for_numeric_range
+times_loop; p $global_var
