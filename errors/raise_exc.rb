ef38a185eb8e0e80ef6ac9c6a23fcd0d191b3f21
Wei Ning <macgnw@gmail.com>
2019-03-26 20:09:05 +1100

    err

--- errors/raise_exc.rb
+++ errors/raise_exc.rb
@@ -1,1 +1,18 @@
-raise ArgumentError, 'basd'
+#!/usr/bin/env ruby
+
+def try_except
+  begin
+    raise ArgumentError, 'basd'
+  rescue ArgumentError => e
+    puts "ArgumentError: #{e}"
+  rescue StandardError => e
+    puts "default error handler: #{e}"
+  end
+end
+
+def main
+  try_except()
+end
+
+main()
+
