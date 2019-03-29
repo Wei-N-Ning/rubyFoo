c2f98d3517832d858ecbb4459f52665a48ae24b0
Wei Ning <wei@canva.com>
2019-03-29 11:59:54 +1100

    rails

--- http/rails/basic/config.ru
+++ http/rails/basic/config.ru
@@ -1,3 +1,5 @@
-require_relative 'config/application'
+# This file is used by Rack-based servers to start the application.
 
-run Basic::Application.new
+require_relative 'config/environment'
+
+run Rails.application
--- http/rails/basic/config/application.rb
+++ http/rails/basic/config/application.rb
@@ -1,17 +1,19 @@
-require 'bundler'
-Bundler.require(:default)
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module Basic
-  class Application < Rail::Application
-    # Import assets from other gems:
-    # config.gems << 'turbolinks'
-    #
-    # Precompile assets using `rake assets`:
-    # config.precompile << 'application.css'
-    # config.precompile << 'application.js'
-    # config.precompile << 'index.html'
-    #
-    # Compress assets:
-    # config.compress = true
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
