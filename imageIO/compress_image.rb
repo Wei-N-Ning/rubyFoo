9d1352a47b1642cf46bf5bbc714507c8e50b5387
Wei Ning <wei@canva.com>
2019-03-30 15:49:44 +1100

    image io

--- imageIO/compress_image.rb
+++ imageIO/compress_image.rb
@@ -4,8 +4,10 @@ require 'image_optimizer'
 
 def main
   `cp ./image.png /var/tmp/image.png`
+  p `ls -larth /var/tmp/image.png`
   imo = ImageOptimizer.new('/var/tmp/image.png', quality: 25)
   p imo.optimize
+  p `ls -larth /var/tmp/image.png`
 end
 
 main
