55a53b3ae9dab9219f28cc2fe940117bc152c5fd
Wei Ning <wei@canva.com>
2019-04-26 15:50:18 +1000

    wip

--- fileIO/open_file.rb
+++ fileIO/open_file.rb
@@ -4,7 +4,7 @@ def open_to_read
   # r+: reading and updating
   # oneliner syntax works also here
   File.open('/etc/ssh/ssh_config', 'r') do |fp|
-    text = fp.read()
+    text = fp.read
     puts text.each_line.to_a.length
   end
 
@@ -13,16 +13,16 @@ def open_to_read
 end
 
 def open_to_write
-  File.open('/var/tmp/acow', 'w') do |fp|
-    fp.write(<<"EOF")
-there is a 
-cow
-there is a cow
-EOF
+  File.open('/var/tmp/sut/acow', 'w+') do |fp|
+    fp.write(<<~"EOF")
+      there is a 
+      cow
+      there is a cow
+      EOF
     fp.puts(%w/there is a cow 113/)  # elements are written per line
     # just like the behavior of puts with stdout
   end
-  puts `ls -larth /var/tmp/acow`
+  puts `ls -larth /var/tmp/sut/acow`
 end
 
 def main
@@ -30,5 +30,4 @@ def main
   open_to_write
 end
 
-main()
-
+main
